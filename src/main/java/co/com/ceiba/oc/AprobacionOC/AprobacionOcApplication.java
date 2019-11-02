2f260bf13e02c6986bd1a13c9eb8bdcc7e891c60
hymura <alex.hymura@gmail.com>
2019-11-02 16:26:56 -0500

    commit de la creacion del proyecto aprobacion OC

---
+++ src/main/java/co/com/ceiba/oc/AprobacionOC/AprobacionOcApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.oc.AprobacionOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprobacionOcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprobacionOcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ceiba/oc/AprobacionOC/AprobacionOcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.oc.AprobacionOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprobacionOcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
