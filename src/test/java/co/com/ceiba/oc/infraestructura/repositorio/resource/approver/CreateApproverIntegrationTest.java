4eb25f3e11e656d16b24240b47f66cec3ca9f217
hymura <alex.hymura@gmail.com>
2019-11-26 11:37:12 -0500

    ignorar prueba de integracion

--- src/test/java/co/com/ceiba/oc/infraestructura/repositorio/resource/approver/CreateApproverIntegrationTest.java
+++ src/test/java/co/com/ceiba/oc/infraestructura/repositorio/resource/approver/CreateApproverIntegrationTest.java
@@ -3,6 +3,7 @@ package co.com.ceiba.oc.infraestructura.repositorio.resource.approver;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import javax.transaction.Transactional;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,22 +20,23 @@ import org.springframework.web.context.WebApplicationContext;
 import co.com.ceiba.oc.AprobacionOcApplication;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest(classes = AprobacionOcApplication.class)
+@Transactional
 public class CreateApproverIntegrationTest {
 	
   @Autowired
     private WebApplicationContext wac;
     private MockMvc mvc;
     
-
-    
+     
     @Before
     public void setup() {
         this.mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
-  
-    @Test
+    
     @Ignore
+    @Test   
     public void processAuditGoodRequest1() throws Exception {
 
         //Arrange
