71783fdb929e1fe7bdb1fc7bee3c3b249b78643a
hymura <alex.hymura@gmail.com>
2019-11-29 09:10:35 -0500

    arreglo de codigo smell

--- src/main/java/co/com/ceiba/oc/AprobacionOcApplication.java
+++ src/main/java/co/com/ceiba/oc/AprobacionOcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AprobacionOcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AprobacionOcApplication.class, args);
+		SpringApplication.run(AprobacionOcApplication.class);
 	}
 
 }
